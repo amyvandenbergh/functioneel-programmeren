e07dbe859d6544411a8ac19e62052bd87e46e0ad
amyvandenbergh <amyvandenbergh@gmail.com>
2021-11-30 12:17:33 +0100

    oefening 1.2

--- oef1.2/1.2.php
+++ oef1.2/1.2.php
@@ -1,36 +1,54 @@
-<?php
-error_reporting( E_ALL );
-ini_set( 'display_errors', 1 );
-
-//3 afbeeldingen in een array
-$images = array( "berlin.jpg" => "Berlijn",
-    "paris.jpg" => "Parijs",
-    "london_2423609b.jpg" => "Londen" );
-
-?>
-
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
+    <title>Mijn eerste webpagina</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
 </head>
 <body>
 
 <div class="jumbotron text-center">
-    <h1>Leuke plekken in Europa</h1>
-    <p>Tips voor citytrips voor vrolijke vakantiegangers!</p>
+  <h1>My First Bootstrap Page</h1>
+  <p>Resize this responsive page to see the effect!</p>
 </div>
 
 <div class="container">
-    <div class="row">
+  <div class="row">
+    <div class="col-sm-4">
+      <h3>Column 1</h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
+      <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
+    </div>
+    <div class="col-sm-4">
+      <h3>Column 2</h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
+      <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
+    </div>
+    <div class="col-sm-4">
+      <h3>Column 3</h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
+      <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
+    </div>
+  </div>
+</div>
 
-        <?php
+</body>
+</html>
+<?php
+//3 afbeeldingen in een array
+$images = [ "berlin.jpg" => "Berlijn",
+                "parijs.jpg" => "Parijs",
+    "londen.jpg" => "Londen" ]
 
-        //loop over de afbeeldingen
+//loop over de afbeeldingen
         foreach ( $images as $image => $title )
         {
             $link_image = "img/$image";
@@ -53,8 +71,3 @@ $images = array( "berlin.jpg" => "Berlijn",
 
         ?>
 
-    </div>
-</div>
-
-</body>
-</html>
